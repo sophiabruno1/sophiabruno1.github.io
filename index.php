1411536ed0ebcf6224f3a67681f36d2913d07cad
sophiabruno1 <sophiabruno1@gmail.com>
2017-04-02 12:16:31 -0400

    meu primeiro commite

---
+++ index.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<title>sophia bruno alves =^.^=</title>
+</head>
+<body>
+
+<h1>My First Heading1</h1>
+<h2                              >My First Heading2</h2>
+<h2 style="background-color:cyan">My First Heading2</h2>
+<h3>My First Heading3</h3>
+<h4>My First Heading4</h4>
+<h5>My First Heading5</h5>
+<h6>My First Heading6</h6>
+<p>minha primeira imagem</p>
+<img src="alicedoodling.jpeg" alt="nao temos uma photo de montanha" style="width:304px;height:228px;">
+<h2 style="background-color:cyan">
+Background-color set by using cyan
+</h2><p>I am normal</p>
+<p style="color:red;">I am red</p>
+</body>
+</html> 
+
